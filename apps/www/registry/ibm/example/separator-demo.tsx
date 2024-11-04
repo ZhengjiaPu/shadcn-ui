43831207f0a34fc7128eb1aa87d36d6f594716fa
ycai0273 <caiyihuan614@gmail.com>
2024-11-05 00:12:37 +1100

    separator component styles stories doc

--- apps/www/registry/ibm/example/separator-demo.tsx
+++ apps/www/registry/ibm/example/separator-demo.tsx
@@ -4,17 +4,19 @@ export default function SeparatorDemo() {
   return (
     <div>
       <div className="space-y-1">
-        <h4 className="text-sm font-medium leading-none">Radix Primitives</h4>
-        <p className="text-sm text-muted-foreground">
+        <h4 className="text-sm font-medium leading-none text-blue-500">
+          Radix Primitives
+        </h4>
+        <p className="text-sm text-blue-400">
           An open-source UI component library.
         </p>
       </div>
-      <Separator className="my-4" />
-      <div className="flex h-5 items-center space-x-4 text-sm">
+      <Separator className="my-4 bg-blue-500" />
+      <div className="flex h-5 items-center space-x-4 text-sm text-blue-500">
         <div>Blog</div>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="bg-blue-500" />
         <div>Docs</div>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="bg-blue-500" />
         <div>Source</div>
       </div>
     </div>
